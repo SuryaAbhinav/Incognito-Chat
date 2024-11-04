3b0f23b613ad5398ade65ec01d3cc19e123c6a89
Surya Abhinav <abhinav97.ch@gmail.com>
2024-11-04 22:14:11 +0900

    Feat: Image for bot, simplified UI, adapting dark and light modes, screens MacOS, iOS and iPads

---
+++ HelloGPT/ColorSchemeManager.swift
@@ -0,0 +1,33 @@
+//
+//  ColorSchemeManager.swift
+//  HelloGPT
+//
+//  Created by Surya Abhinav on 2024/11/04.
+//
+
+
+import SwiftUI
+
+struct ColorSchemeManager {
+    static func backgroundColor(for colorScheme: ColorScheme) -> Color {
+        colorScheme == .dark ? .black.opacity(0.2) : .white.opacity(0.2)
+    }
+
+    static func userMsgColor(for colorScheme: ColorScheme) -> Color {
+        colorScheme == .dark ? .black.opacity(0.4) : .gray.opacity(0.2)
+    }
+
+    static func codeBlockColor(for colorScheme: ColorScheme) -> Color {
+        colorScheme == .dark ? .black.opacity(0.2) : .gray.opacity(0.2)
+    }
+    
+//    static func textEditorBackground(for colorScheme: ColorScheme) -> Color {
+//        colorScheme == .dark ? .clear : .clear
+//    }
+    
+    static func clearBackground(for colorScheme: ColorScheme) -> Color {
+        colorScheme == .dark ? .clear : .clear
+    }
+    
+    // Additional color methods as needed
+}
